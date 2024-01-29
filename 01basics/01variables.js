5e81264c765316dd38bce5b7ed82bf097b82783a
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-01-29 19:18:09 +0530

    datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(2+2) //wwe are using nodejs, not browser
+
+console.log(2 + 2); //code readability should be hight
+
+console.log("shanu");
+
+let name = "shanu";
+let age = 18;
+let isLogged = false;
+let state;
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
