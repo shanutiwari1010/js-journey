21ebbec7d4398f88c8776569d30f9fc8ff95308c
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-01-29 19:33:40 +0530

    conversion of datatypes

---
+++ 01basics/02datatypeConversion.js
@@ -0,0 +1,17 @@
+let age = undefined;
+
+// console.log(typeof age)
+
+let valueInNumber = Number(age)
+// console.log(parseInt(age))
+// console.log(typeof parseInt(age))
+
+console.log(typeof valueInNumber)
+console.log( valueInNumber) //Nan (not a number)
+
+
+
+// ----------------*****************--------------//
+//  "10" => 10
+//  "10abc" => NaN
+//  "true" => 1; "false " => 0
