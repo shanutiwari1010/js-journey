1a5f12cffa9f49594be3f44452a28049a64c14bb
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-01-29 20:12:16 +0530

    == and === uses

--- 01basics/02datatypeConversion.js
+++ 01basics/02datatypeConversion.js
@@ -2,29 +2,36 @@ let age = undefined;
 
 // console.log(typeof age)
 
-let valueInNumber = Number(age)
+let valueInNumber = Number(age);
 // console.log(parseInt(age))
 // console.log(typeof parseInt(age))
 
-console.log(typeof valueInNumber)
-console.log( valueInNumber) //Nan (not a number)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //Nan (not a number)
 
-console.log("8" +2+4) //824
-console.log(5 +2+"4")  //74
+console.log("8" + 2 + 4); //824
+console.log(5 + 2 + "4"); //74
+
+console.log("****************************");
+console.log(null == 0); //false
+console.log(null > 0); //false
+console.log(null < 0); //false
+console.log(null >= 0); //true
+console.log(null >= 0); //true
+
+/*----------------***********  NOTES-CONVERSION  ***********--------------
+ "10" => 10
+ "10abc" => NaN
+"true" => 1; "false " => 0
 
 
+**********  Primitive  ***********
+7 types : string, number, boolean, null , undefined, symbol, Bigint
 
-// ----------------*********NOTES-CONVERSION**********--------------//
-//  "10" => 10
-//  "10abc" => NaN
-//  "true" => 1; "false " => 0
+**********  NON Primitive ***********
+Arrays , Objects, functions
 
 
-/// ************** NOTES-CONPARISION**************///
-console.log ("****************************")
-console.log(null ==0) //false
-console.log(null >0)  //false
-console.log(null <0)  //false
-console.log(null >=0) //true
-console.log(null >=0) //true
+----JS is Dynamically typed language--
 
+*/
