c32c678e0f7421201a3ea0dbf49d20d8f917e35b
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-02-02 22:22:04 +0530

    project 3 solution code

---
+++ 07projects/03_digitalClock/script.js
@@ -0,0 +1,9 @@
+document.getElementById("clock");
+
+setInterval(() => {
+  let date = new Date();
+
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
+
+// setInterval method takes method and time interval for how much time it should be run
