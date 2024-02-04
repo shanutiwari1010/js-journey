0dc2aa7af2388db0091a423fe27ae277b7310451
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-02-04 12:29:21 +0530

    this keyword

---
+++ 10oops/prototype.js
@@ -0,0 +1,63 @@
+// let myName = "shanu       "
+
+// console.log(myName.length)
+
+
+let myHeros = ["thor", "spiderman"]
+
+let heroPower = {
+    thor : "hammer",
+    spiderman : "sling",
+
+    getSpiderPower: function(){
+        console.log(`spidy powerbis ${this.spiderman}`)
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`);
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`${this.name}`)
+    console.log(`True length is : ${this.trim().length}`)
+
+}
+
+anotherUsername.trueLength()
